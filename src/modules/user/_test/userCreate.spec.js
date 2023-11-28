022d0a9c2a2b777b4a57459b1591ba39c9647489
MlimaU <129126676+mlimau@users.noreply.github.com>
2023-11-28 15:28:00 -0500

    First test 3 steps (user create, delete,get)

--- src/modules/user/_test/userCreate.spec.js
+++ src/modules/user/_test/userCreate.spec.js
@@ -33,72 +33,19 @@ describe('USER CREATE', () => {
                     const respData = res.body.data//5 что придёт response.body помещаем в переменную respData
                     userId = respData.userCreate._id
 
-                    console.log("RESP BODY ===", respData)//data выйдет сюда используем в следующих тестах
-                    expect(respData.userCreate.firstName).eq('firstTestFName')//path and expected parameters
+                    console.log("RESP BODY ===", res.body)//data выйдет сюда используем в следующих тестах
+                    expect(respData.userCreate.firstName).eq('firstTestFName')//path from Operation Appolo and expected parameters
                     expect(respData.userCreate.lastName).eq('firstTestLName')
                     expect(respData.userCreate._id).to.be.a('string')
-
                     done()//6
                 })
         })
-
-        it('Created user deleting', () => {
-
-            const argReq = {//// Use the userId from the first test
-                userId
-            }
-            console.log(userId)
-            const responseData = {
-                query:
-                    `mutation UserDeleteById($userId: ID!) {
-                    userDeleteById(userId: $userId)
-                }`,
-
-                variables: argReq,//request const from Apollo
-            }
-            request(graphQLEndpoint)//1 call our cont with endpoint, open page
-                .post('/').send(responseData).expect(200)
-                .end((err, resp) => {
-                    if (err) return done(err)
-                    const response = resp.body
-                    console.log(response)
-                    expect(response.body.userDeleteById).to.be.true
-                    done()
-
-                })
-        })
-
-        it('Get deleted user', () => {
-            request(graphQLEndpoint)
-                .post('/')
-                .send({
-                    query: `UserGetById($userId: ID!) {
-                userGetById(userId: $userId) {
-                    _id
-                    firstName
-                    lastName
-                }
-            }`,variables: {
-                        userId
-                    }
-                })
-                .expect(200)
-                .end((err, res) => {
-                    if (err) return done(err);
-                    const response = res.body;
-                    if (response.errors) {
-                        // Assuming you want to check if the errors contain a specific string
-                        expect(response.errors.message).to.include("Cannot");
-                    }
-                    done()
-                })
-            //test run through package.json scripts-test, we shouls correct the name/path of /userCreate.spec.js
-
-            describe('USER CREATE NEGATIVE', () => {///
-
-
-            })
-        })
-
     })
 })
+
+
+
+
+
+
+
---
+++ src/modules/user/_test/userGetByIdAndDelete.spec.js
@@ -0,0 +1,99 @@
+const request = require ('supertest')
+const { expect } = require('chai')
+const graphqlEndPoint = 'http://localhost:5000/graphql'
+
+
+describe('Positives user tests', () => {
+    describe('USER GET BY ID', () => {
+        let userId
+        it('Created user get by ID', (done) => {
+
+    const reqData = {
+            "userInput": {
+              "firstName": "User1FirstName",
+              "lastName": "User1LastName"
+              }
+            }
+    const respData = {
+                query: `mutation UserCreate($userInput: UserItems) {
+          userCreate(userInput: $userInput) {
+            _id
+            firstName
+            lastName
+          }
+     }`, variables: reqData
+            }
+
+        request(graphqlEndPoint)
+           .post('/')
+            .send(respData)
+            .expect(200)
+            .end((err, res) => {
+                if(err) return done(err)
+
+     const responseData = res.body.data
+     userId = responseData.userCreate._id
+     console.log(responseData)
+                console.log("USER ID ===",responseData.userCreate._id)
+                done()
+            })
+        })//above user was created without assertions
+
+        it('Get user by ID', (done) => {
+            const requestData = {
+               userId: userId
+            }
+            const responseData = {
+                query: `query UserGetById($userId: ID!) {
+                userGetById(userId: $userId) {
+                    _id
+                    firstName
+                    lastName
+                }
+            }`,
+                variables: requestData
+            }
+
+            request(graphqlEndPoint)
+                .post('/')
+                .send(responseData)
+                .expect(200)
+                .end((err, res) => {
+                    if(err) return done(err)
+                    const responseData = res.body.data
+                    console.log("RESP DATA USER GET BY ID ===", responseData)
+                    expect(responseData.userGetById._id).eq(userId)//path from Apollo Operation - res.body.data+path
+                    expect(responseData.userGetById.firstName).eq('User1FirstName')
+                    done()
+                })
+        })
+        it('Delete created user', (done) => {
+            const arq = {
+                "userId": userId
+            }
+
+            const resBody = {
+                query: `mutation UserDeleteById($userId: ID!) {
+  userDeleteById(userId: $userId)
+}`,
+                variables: arq
+            }
+
+            request(graphqlEndPoint)
+                .post('/')
+                .send(resBody)
+                .expect(200)
+                .end((err, res) => {
+                    if(err) return done(err)
+
+            const respon = res.body.data
+            console.log("USER DELETE ===", respon)
+
+            expect(respon.userDeleteById).to.eq(true)
+            done()
+                })
+        })
+
+
+    })
+})
